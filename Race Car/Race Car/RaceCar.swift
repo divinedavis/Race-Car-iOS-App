51a272716153e1101e2d478676eb2bcbb2195674
divinedavis <divinejdavis@gmail.com>
2015-09-21 18:47:19 -0400

    Done with the app - Prints "Honk! Honk!" to the debugger - Uses label (3) - Uses button - Uses a class file

---
+++ Race Car/Race Car/RaceCar.swift
@@ -0,0 +1,20 @@
+//
+//  RaceCar.swift
+//  Race Car
+//
+//  Created by Divine Davis on 9/21/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import UIKit
+
+class RaceCar: NSObject {
+
+    var brand : String = "Ferrari"
+    var color : String = "Red"
+    var topSpeed : Int = 200
+    
+    func honk() {
+        print("Honk Honk!")
+    }
+}
--- Race Car/Race Car/ViewController.swift
+++ Race Car/Race Car/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var topSpeedLabel: UILabel!
+    
+    @IBAction func honkTapped(sender: AnyObject) {
+        let myCar = RaceCar()
+        myCar.honk()
+        brandLabel.text = myCar.brand
+        colorLabel.text = myCar.color
+        topSpeedLabel.text = "\(myCar.topSpeed)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
