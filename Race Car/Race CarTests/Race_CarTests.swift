6fbbc0edd35d2e46dc1cb49c788b1b9308abb1e6
divinedavis <divinejdavis@gmail.com>
2015-09-21 18:13:33 -0400

    First commit of the files

---
+++ Race Car/Race CarTests/Race_CarTests.swift
@@ -0,0 +1,36 @@
+//
+//  Race_CarTests.swift
+//  Race CarTests
+//
+//  Created by Divine Davis on 9/21/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import XCTest
+@testable import Race_Car
+
+class Race_CarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
